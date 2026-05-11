b65efb19d281ebc07993c2bbf8cd3e0392bcf568
alexandre944-8537 <alexandre944@gmail.com>
2026-05-11 20:46:50 -0300

    70% concluido ajuste link

--- FrontEnd/src/environments/environment.prod.ts
+++ FrontEnd/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: 'https://api.neritechauto.com.br/api',
-  supportUrl: 'https://api.neritechauto.com.br/api',
+  supportUrl: 'https://api.neritechauto.com.br:8081',
   useHash: false,
 };
