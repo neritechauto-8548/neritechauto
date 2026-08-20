14387fdab1abaa5ec3ce79ec0d5a6d3c3a338f5c
neritechauto-8548 <neritechauto@gmail.com>
2026-08-20 15:49:25 -0300

    security: export permission route guard

--- FrontEnd/src/app/core/authentication/index.ts
+++ FrontEnd/src/app/core/authentication/index.ts
@@ -1,5 +1,6 @@
 export * from './interface';
 export * from './auth-guard';
+export * from './permission-guard';
 export * from './subscription-guard';
 export * from './auth.service';
 export * from './token-factory.service';
