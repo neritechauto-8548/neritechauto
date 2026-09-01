2136cfb2c72c405c20115427f0dff6cc27ca0732
neritechauto-8548 <neritechauto@gmail.com>
2026-09-01 17:18:50 -0300

    feat(authz): proteger rotas de ordem de servico

--- FrontEnd/src/app/routes/os/os.routes.ts
+++ FrontEnd/src/app/routes/os/os.routes.ts
@@ -1,18 +1,51 @@
 import { Routes } from '@angular/router';
+import { permissionGuard } from '@core';
 import { OrdemServico } from './ordem-servico/ordem-servico';
 import { CadastroOS } from './cadastro-os/cadastro-os';
 
 export const routes: Routes = [
-  { path: '', component: OrdemServico },
-  { path: 'cadastro', component: CadastroOS },
-  { path: 'cadastro/:id', component: CadastroOS },
-  { path: 'visualizar-editar-os/:numero', loadComponent: () => import('./visualizar-os/visualizar-os').then(m => m.VisualizarOS) },
-  // Alias para compatibilidade com navegação "Visualizar OS"
-  { path: 'visualizar-os/:numero', loadComponent: () => import('./visualizar-os/visualizar-os').then(m => m.VisualizarOS) },
-  // Rotas sem parâmetro para cliques que não enviam :numero
-  { path: 'visualizar-editar-os', loadComponent: () => import('./visualizar-os/visualizar-os').then(m => m.VisualizarOS) },
-  { path: 'visualizar-os', loadComponent: () => import('./visualizar-os/visualizar-os').then(m => m.VisualizarOS) },
-  // Redirecionamentos para cobrir variações de caminho
+  {
+    path: '',
+    component: OrdemServico,
+    canActivate: [permissionGuard],
+    data: { title: 'Ordens de Serviço', permissions: ['GERAL_USUARIO'] },
+  },
+  {
+    path: 'cadastro',
+    component: CadastroOS,
+    canActivate: [permissionGuard],
+    data: { title: 'Nova Ordem de Serviço', permissions: ['OS_INCLUIR'] },
+  },
+  {
+    path: 'cadastro/:id',
+    component: CadastroOS,
+    canActivate: [permissionGuard],
+    data: { title: 'Editar Ordem de Serviço', permissions: ['OS_EDITAR'] },
+  },
+  {
+    path: 'visualizar-editar-os/:numero',
+    canActivate: [permissionGuard],
+    data: { title: 'Visualizar Ordem de Serviço', permissions: ['GERAL_USUARIO'] },
+    loadComponent: () => import('./visualizar-os/visualizar-os').then(m => m.VisualizarOS),
+  },
+  {
+    path: 'visualizar-os/:numero',
+    canActivate: [permissionGuard],
+    data: { title: 'Visualizar Ordem de Serviço', permissions: ['GERAL_USUARIO'] },
+    loadComponent: () => import('./visualizar-os/visualizar-os').then(m => m.VisualizarOS),
+  },
+  {
+    path: 'visualizar-editar-os',
+    canActivate: [permissionGuard],
+    data: { title: 'Visualizar Ordem de Serviço', permissions: ['GERAL_USUARIO'] },
+    loadComponent: () => import('./visualizar-os/visualizar-os').then(m => m.VisualizarOS),
+  },
+  {
+    path: 'visualizar-os',
+    canActivate: [permissionGuard],
+    data: { title: 'Visualizar Ordem de Serviço', permissions: ['GERAL_USUARIO'] },
+    loadComponent: () => import('./visualizar-os/visualizar-os').then(m => m.VisualizarOS),
+  },
   { path: 'visualizar', redirectTo: 'visualizar-os', pathMatch: 'full' },
   { path: 'visualizar-editar', redirectTo: 'visualizar-os', pathMatch: 'full' },
 ];
